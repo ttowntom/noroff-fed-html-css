0a6857d130542ec75624d22cf1f066b25a38e709
ttowntom <117744552+ttowntom@users.noreply.github.com>
2023-05-31 11:17:55 +0200

    updated gallery

--- gallery.js
+++ gallery.js
@@ -1,24 +1,22 @@
-// Suggested by ChatGTP
+// Suggested by ChatGTP - after some back and forth :P
 
 $(document).ready(function () {
-	// Get the hero image element
+	// 	// Get the hero image element
 	var heroImg = $("#hero--img-stormChaser");
-
-	// Get the thumbnail images
+	// 	// Get the thumbnail images
 	var thumbnails = $(".thumbnails img");
 
 	// Add click event handler for each thumbnail
 	thumbnails.click(function () {
 		// Remove the 'active' class from all thumbnails
 		thumbnails.removeClass("active");
-
 		// Add the 'active' class to the clicked thumbnail
 		$(this).addClass("active");
 
 		// Get the source of the clicked thumbnail
 		var newImageSrc = $(this).attr("src");
 
-		// Update the background image of the hero image
-		heroImg.css("background-image", "url(" + newImageSrc + ")");
+		// Update the custom property value
+		heroImg.css("--background-img", "url(" + newImageSrc + ")");
 	});
 });
